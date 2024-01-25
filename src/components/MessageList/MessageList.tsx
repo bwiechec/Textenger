c7db7779c2f09cefcb36780bc4c73e3146a3f8d6
Bartosz Wiecheć <32521016+bwiechec@users.noreply.github.com>
2024-01-25 17:00:45 +0100

    change max viewport units

--- src/components/MessageList/MessageList.tsx
+++ src/components/MessageList/MessageList.tsx
@@ -60,7 +60,7 @@ export default function MessageList() {
 
   return (
     <ThreadContextProvider value={threadData}>
-      <div className="min-h-full w-full max-w-full max-h-[100svh] items-center flex pt-4 relative flex-col">
+      <div className="min-h-full w-full max-w-full h-[100svh] items-center flex pt-4 relative flex-col">
         <span className="text-xl">{threadData?.name}</span>
         <LoaderOverlay status={statusThread} />
         <div className="h-full w-full flex p-4 relative flex-col-reverse overflow-y-auto max-w-full">
